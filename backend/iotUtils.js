161ba1eb100bbdb987b9521dd0177894d72cf19e
tech4GT <varun.gupta1798@gmail.com>
2018-03-31 13:16:02 +0530

    starting iot
    Signed-off-by: tech4GT <varun.gupta1798@gmail.com>

--- backend/iotUtils.js
+++ backend/iotUtils.js
@@ -1,19 +1,28 @@
 const five = require('johnny-five')
+const ras = require('raspi-io');
+var board = new five.Board({
+    io : new ras()
+});
+board.on('ready',function(){
+    led = new five.Led('P1-13');
+    led.on();
+});
 
 module.exports = {
     fetchAllStatus: function () { 
         return new Promise((resolve,reject)=>{
+            led.off()
             resolve([false,false])
         })
-     },
+    },
     fetchStatusbyId: function (id) {
-         return new Promise((resolve,reject)=>{
-             resolve(false)
-         })
-       },
+        return new Promise((resolve,reject)=>{
+            resolve(false)
+        })
+    },
     setStatusbyId: function (id,status) { 
         return new Promise((resolve,reject)=>{
             resolve(status)
         })
-     }
+    }
 }
